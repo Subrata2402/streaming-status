843fa3f2015b797d68e01df741c0f231ab9f058b
Sakhman <68939201+Sakhman@users.noreply.github.com>
2020-11-01 14:24:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def on_ready():
     	await bot.change_presence(activity=discord.Activity(type=1,name="with all members !"))
     	await asyncio.sleep(5)
 
-        await bot.change_presence(activity=discord.Activity(type=1,name="with want HQ Life in 1.2rs DM me !"))
+        await bot.change_presence(activity=discord.Activity(type=1,name="with want HQ Life DM me !"))
     	await asyncio.sleep(5)
     	
     	await bot.change_presence(activity=discord.Activity(type=1,name="with  Trivia Master || Bots !"))
